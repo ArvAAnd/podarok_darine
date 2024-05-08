a91849e82dad50009da9c39d4621139d8905c3c4
Arsen_home <arsenvakhidov2005@gmail.com>
2024-05-08 21:08:19 +0300

    no button static hover = absolute

--- src/App.tsx
+++ src/App.tsx
@@ -3,28 +3,32 @@ import './App.css';
 import { kittyList } from './kitty';
 import { setTextRange } from 'typescript';
 
-
+type Position = 'absolute' | 'fixed' | 'relative' | 'static' | 'sticky';
 function App() {
   const [topState, setTopState] = useState("740px");
   const [leftState, setLeftState] = useState("860px");
   const [imageCur, setImageCur] = useState(kittyList[1].image);
   const [text, setText] = useState("У меня подарок для самой красивой девочки на свете, это вы?")
+  
+  const [posState, setPosState] = useState('static')
   function getRandomInt(max: number) {
     return Math.floor(Math.random() * max);
   }
   
   const noFunc = () => {
+    setPosState('absolute')
     setTopState(`${getRandomInt(900)}px`)
     setLeftState(`${getRandomInt(1000)}px`)
   }
   const yesFunc = () => {
+    setPosState('static')
     setImageCur(kittyList[2].image);
     setText("Тогда это вам! С днем рождения! Арсен передал, что очень любит вас")
     
   }
   const lox = () => {
     setImageCur(kittyList[0].image);
-    setTopState(`770px`)
+    setPosState('static')
     setText("Ему бы было очень неприятно такое слышать, но в такой день он вас прощает, отвечайте на вопрос: Вы самая красивая девочка на свете?")
   }
   return (
@@ -35,7 +39,14 @@ function App() {
       </div>
       <div className='buttons'>
         <button className='button' onClick={yesFunc}>Да</button>
-        <button className='button no' onMouseOver={noFunc} style={{ top: `${topState}`, left: `${leftState}`}}>Нет</button>
+        <button 
+          className='button no' 
+          onMouseOver={noFunc} 
+          style={{
+              top: `${topState}`, 
+              left: `${leftState}`, 
+              position: posState as Position
+            }}>Нет</button>
         <button className='button' onClick={lox}>Арсен лох</button>
       </div>
     </div>
